abcf88ee943626a4f1ed844a1d6d696a73efa6c9
Paulex <paulalexis.verrier@ynov.com>
2023-03-08 13:53:49 +0100

    Initial Commit

---
+++ GoofBox/GoofBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoofBoxApp.swift
+//  GoofBox
+//
+//  Created by Paul-Alexis Verrier on 08/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct GoofBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
